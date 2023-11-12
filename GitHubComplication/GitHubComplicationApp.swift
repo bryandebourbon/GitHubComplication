8e331ebcad8190721cd83b3eebacd4c32e95d04d
Bryan de Bourbon <bryandebourbon@gmail.com>
2023-11-11 21:12:20 -0500

    Initial Commit

---
+++ GitHubComplication/GitHubComplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubComplicationApp.swift
+//  GitHubComplication
+//
+//  Created by Bryan de Bourbon on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubComplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GitHubComplicationUITests/GitHubComplicationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  GitHubComplicationUITestsLaunchTests.swift
+//  GitHubComplicationUITests
+//
+//  Created by Bryan de Bourbon on 11/11/23.
+//
+
+import XCTest
+
+final class GitHubComplicationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
